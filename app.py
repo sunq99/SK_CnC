f0650acfb2eab947eb6943bf32b92e7bd94454e7
sunq99 <edwin9903085@gmail.com>
2025-05-23 12:45:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 import json
 
 import openai
-from openai import OpenAI
+#from openai import OpenAI
 from config import OPENAI_API_KEY
 from intent_analysis import intent_analysis
 from generate_multiquery_and_retrieve import generate_multiquery_and_retrieve
@@ -19,8 +19,7 @@ from langchain.chat_models import ChatOpenAI
 from sentence_transformers import CrossEncoder
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-openai.api_key = OPENAI_API_KEY
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def clean_incomplete_sentences(content):
     """

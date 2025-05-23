b5a3ddd25d3664f575e8ca95431bd83802a170d4
sunq99 <edwin9903085@gmail.com>
2025-05-23 13:23:53 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import streamlit as st
-
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-FAISS_INDEX_PATH = st.secrets["FAISS_INDEX_PATH"]
+%%writefile config.py
+OPENAI_API_KEY = "OPENAI_API_KEY"
+FAISS_INDEX_PATH = "/content/faiss_index_path"
+# CSV_PATH = "/content/저작권법 조문.csv"

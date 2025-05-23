70bd8c95bdb015837a7b951e161d12a62715dce8
sunq99 <edwin9903085@gmail.com>
2025-05-23 12:09:32 +0900

    Update generate_answer_and_evaluate.py

--- generate_answer_and_evaluate.py
+++ generate_answer_and_evaluate.py
@@ -8,7 +8,7 @@ from ragas import EvaluationDataset, evaluate
 from ragas.llms import LangchainLLMWrapper
 from ragas.metrics import Faithfulness, ResponseRelevancy
 
-os.environ["OPENAI_API_KEY"] = "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"
+openai_api_key=OPENAI_API_KEY
 
 ragas_llm = ChatOpenAI(model="gpt-4o")
 ragas_embeddings = OpenAIEmbeddings()

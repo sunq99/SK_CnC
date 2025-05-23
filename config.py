e3effed9969f9925615e94096054e6ce5ca11ad5
sunq99 <edwin9903085@gmail.com>
2025-05-23 10:08:03 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 FAISS_INDEX_PATH = "/content/faiss_index_path"
 # CSV_PATH = "/content/저작권법 조문.csv"
